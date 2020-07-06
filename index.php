bf960cf835fcdfb3ad3d67a5fd88d65e017ae360
Kalebe Gouvea <k.gouvea@live.com>
2020-07-06 20:53:01 -0300

    Add teste if para idade

--- index.php
+++ index.php
@@ -4,4 +4,22 @@ $categorias = [];
 $categorias[] = 'infantil';
 $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
-print_r($categorias);
+//print_r($categorias);
+
+$nome = 'Samuel';
+$idade = 22;
+
+if ($idade >= 6 && $idade <= 12) 
+{
+    echo 'infantil';
+}
+
+elseif ($idade >= 13 && $idade <= 18) 
+{
+    echo 'adolescente';
+}
+
+else 
+{
+    echo 'adulto';
+}
