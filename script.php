b43d03fee852d5c4f018df3ae1d29fdebd155ab7
Kalebe Gouvea <k.gouvea@live.com>
2020-07-07 17:55:34 -0300

    Validando dados de

--- script.php
+++ script.php
@@ -9,6 +9,21 @@ $categorias[] = 'adulto';
 $nome = $_POST["nome"];
 $idade = $_POST["idade"];
 
+if (empty($nome)) {
+    echo "O nome não pode ser vazio.";
+    return;
+}
+
+if (strlen($nome) < 3) {
+    echo "O nome não pode ser inferior a 3 caracteres";
+    return;
+}
+
+if (strlen($nome) > 40) {
+    echo "O nome não pode ser superior a 40 caracteres";
+    return;
+}
+
 if ($idade >= 6 && $idade <= 12) {
     for ($i = 0; $i < count($categorias); $i++){
         if ($categorias[$i] == 'infantil'){
