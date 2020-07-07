3cb21d26ec0dc280269e72369dd78189b2ffea64
Kalebe Gouvea <k.gouvea@live.com>
2020-07-07 15:28:12 -0300

    Add index.php do exer02

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Formulário de inscrição</title>
+        <meta name="author" content="">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+        <form action="script.php" method="POST">
+            <p>Your name: <input type="text" name="nome"></p>
+            <p>Your age: <input type="text" name="idade"></p>
+            <p><input type="submit"></p>
+        </form>
+    </body>
+</html>
