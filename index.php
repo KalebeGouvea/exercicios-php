a2e6a8252f6ade6ed7be769045e065c92c361395
Kalebe Gouvea <k.gouvea@live.com>
2020-07-07 12:21:27 -0300

    Finalizado teste if para idade

--- index.php
+++ index.php
@@ -7,19 +7,34 @@ $categorias[] = 'adulto';
 //print_r($categorias);
 
 $nome = 'Samuel';
-$idade = 22;
+$idade = 6;
 
-if ($idade >= 6 && $idade <= 12) 
-{
-    echo 'infantil';
+if ($idade >= 6 && $idade <= 12) {
+    for ($i = 0; $i < count($categorias); $i++){
+        if ($categorias[$i] == 'infantil'){
+            echo "O nadador " .$nome. " compete na categoria infantil";
+        }
+    }
 }
 
 elseif ($idade >= 13 && $idade <= 18) 
 {
-    echo 'adolescente';
+    for ($i = 0; $i < count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'adolescente')
+        {
+            echo "O nadador " .$nome. " compete na categoria adolescente";
+        }
+    }
 }
 
 else 
 {
-    echo 'adulto';
+    for ($i = 0; $i < count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'adulto')
+        {
+            echo "O nadador " .$nome. " compete na categoria adulto";
+        }
+    }
 }
