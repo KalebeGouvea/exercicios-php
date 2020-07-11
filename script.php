0ef9bb68926ad5a267c978c5436832548b7c20ac
Kalebe Gouvea <k.gouvea@live.com>
2020-07-11 15:10:48 -0300

    Add include no script.php

--- script.php
+++ script.php
@@ -1,6 +1,11 @@
 <?php
 
-session_start();
+include "servicos/servicoValidacao.php";
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoCategoriaCompetidor.php";
 
 $nome = $_POST["nome"];
 $idade = $_POST["idade"];
+
+definirCategoriaCompetidor ($nome, $idade);
+header("location: index.php")
